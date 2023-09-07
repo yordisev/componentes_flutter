1f3185392bf466c30f93fb69a32cebb0aa70035f
yordisev <yordys12@gmail.com>
2023-09-07 09:46:04 -0500

    terminar

--- lib/screens/alertas_screen.dart
+++ lib/screens/alertas_screen.dart
@@ -1,15 +1,16 @@
-import 'package:componentes/theme/app_tema.dart';
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertasScreen extends StatelessWidget {
   const AlertasScreen({Key? key}) : super(key: key);
-  void verdialogo(BuildContext context) {
-    showDialog(
-      barrierDismissible: true,
+  void verdialogoiphone(BuildContext context) {
+    showCupertinoDialog(
+      barrierDismissible: false,
       context: context,
       builder: (context) {
-        return AlertDialog(
-            elevation: 5,
+        return CupertinoAlertDialog(
             title: const Text('Gestionar Titulo'),
             content: const Column(
               mainAxisSize: MainAxisSize.min,
@@ -28,8 +29,60 @@ class AlertasScreen extends StatelessWidget {
                 onPressed: () {
                   Navigator.pop(context);
                 },
-                child: const Text('Cancel'),
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Aceptar'),
               )
+            ]);
+      },
+    );
+  }
+
+  void verdialogo(BuildContext context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+            elevation: 5,
+            title: const Text('Gestionar Titulo'),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusDirectional.circular(15)),
+            content: const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('este es otro texto'),
+                SizedBox(
+                  height: 10,
+                ),
+                FlutterLogo(
+                  size: 100,
+                )
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('OK'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
             ]);
       },
     );
@@ -44,7 +97,9 @@ class AlertasScreen extends StatelessWidget {
             //     backgroundColor: AppTema.primary,
             //     shape: const StadiumBorder(),
             //     elevation: 0),
-            onPressed: () => verdialogo(context),
+            onPressed: () => Platform.isAndroid
+                ? verdialogo(context)
+                : verdialogoiphone(context),
             child: const Padding(
               padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
               child: Text(
